0af73a41432fb8dcef8a5a34f717f14eccdabab2
Albert de Haro <Albert.deharo.murcia@gmail.com>
2021-11-24 18:20:59 +0100

    first commit + got houses mocks data added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import houses from './../../Mocks/houses.json'
+import './home.scss';
+
+function Home() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  console.log(houses);
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://anapioficeandfire.com/api/houses/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(houses);
+        },
+        // Nota: es importante manejar errores aquí y no en 
+        // un bloque catch() para que no interceptemos errores
+        // de errores reales en los componentes.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+  console.log(items);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name} {item.price}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Home;
